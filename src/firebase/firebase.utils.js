3b418257dd6c073a4b9cb126bee2ae371219d119
Russell Hart <russellbot@gmail.com>
2021-03-22 16:10:20 -0400

    change firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+var config = {
+  apiKey: "AIzaSyBW_PoyWvmsnx2io-bqdWaKUy3_YA1lHQc",
+  authDomain: "rsty-clothing.firebaseapp.com",
+  projectId: "rsty-clothing",
+  storageBucket: "rsty-clothing.appspot.com",
+  messagingSenderId: "44563655472",
+  appId: "1:44563655472:web:10616ff214dba30f3bf6f5",
+  measurementId: "G-1412EJRY73"
 };
 
 firebase.initializeApp(config);
