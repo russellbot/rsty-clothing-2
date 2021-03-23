f76e959b71995343e9a1b1cf4982c37b103f8de0
Russell Hart <russellbot@gmail.com>
2021-03-23 16:21:01 -0400

    fixed cart counter and connected graphql with flowright

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,6 +1,6 @@
 import { gql } from "apollo-boost";
 
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, getCartItemCount } from "./cart.utils";
 
 export const typeDefs = gql`
   extend type Item {
@@ -18,6 +18,12 @@ const GET_CART_HIDDEN = gql`
     cartHidden @client
   }
 `;
+
+const GET_ITEM_COUNT = gql`
+  {
+    itemCount @client
+  }
+`
 
 const GET_CART_ITEMS = gql`
   {
@@ -46,6 +52,11 @@ export const resolvers = {
       });
 
       const newCartItems = addItemToCart(cartItems, item);
+
+      cache.writeQuery({
+        query: GET_ITEM_COUNT,
+        data: { itemCount: getCartItemCount(newCartItems) }
+      });
 
       cache.writeQuery({
         query: GET_CART_ITEMS,
